b0df71e56e2e7abfd613391cb1bd9786c6fc661d
Tissam <tissam2405@gmail.com>
2026-05-21 17:28:48 +0200

    Fallback connexion production

--- config/database.php
+++ config/database.php
@@ -1,24 +1,29 @@
 <?php
 
-$host = "localhost";
-$port = "5432";
-$dbname = "vite_gourmand";
-$user = "tissam";
-$password = "tissam24";
+$host="localhost";
+$port="5432";
+$dbname="vite_gourmand";
+$user="tissam";
+$password="password";
 
-try {
+try{
 
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname",
-        $user,
-        $password
-    );
+$pdo=
+new PDO(
+"pgsql:host=$host;port=$port;dbname=$dbname",
+$user,
+$password
+);
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(
+PDO::ATTR_ERRMODE,
+PDO::ERRMODE_EXCEPTION
+);
 
-    
+}
 
-} catch (PDOException $e) {
+catch(PDOException $e){
 
-    die("Erreur connexion : " . $e->getMessage());
+$pdo=null;
+
 }
